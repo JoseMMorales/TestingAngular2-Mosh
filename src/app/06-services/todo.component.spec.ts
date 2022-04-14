874217510f762718fe540f0fa035bba6fb67bb79
JoseMMorales <josemulamorales33@hotmail.com>
2022-04-14 18:20:22 +0200

    Testing confirmation boxes

--- src/app/06-services/todo.component.spec.ts
+++ src/app/06-services/todo.component.spec.ts
@@ -1,17 +1,20 @@
 import { TodoComponent } from "./todo.component";
 import { TodoService } from './todo.service';
-import { from, of } from "rxjs";
+import { from, Observable, of } from "rxjs";
 import { todo } from "./todo.interface";
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let service: TodoService;
+  let observable: Observable<any>;
 
   beforeEach(() => {
     const spy = jasmine.createSpyObj('HttpClient', { post: of({}), get: of({}) });
 
     service = new TodoService(spy);
     component = new TodoComponent(service);
+
+    observable = jasmine.createSpyObj('Observable', ['subscribe']);
   })
 
   it('should set todos property with the items returned from the server', () => {
@@ -37,11 +40,28 @@ describe('TodoComponent', () => {
   })
 
   it('should call the server to save the changes when a new item is saved', () => {
-    let observable = jasmine.createSpyObj('Observable', ['subscribe']);
     let spy = spyOn(service, 'add').and.returnValue(observable);
 
     component.add();
     expect(spy).toHaveBeenCalled();
   })
+
+  it('should call the server to delete a todo item if the user confirms', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(observable);
+
+    component.delete(1);
+    expect(spy).toHaveBeenCalledWith(1);
+  })
+
+  it('should NOT call the server to delete a todo item if the user cancels', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(observable);
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalled();
+  })
+
 })
 
