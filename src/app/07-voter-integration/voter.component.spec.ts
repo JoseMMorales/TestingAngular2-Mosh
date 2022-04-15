2fbb8972e564fca0a17ac20224e38a862d14d6fb
JoseMMorales <josemulamorales33@hotmail.com>
2022-04-15 19:04:25 +0200

    Testing properties and class Bindings

--- src/app/07-voter-integration/voter.component.spec.ts
+++ src/app/07-voter-integration/voter.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { VoterComponent } from "./voter.component";
-
 
 describe('VoterComponent', () => {
   let component: VoterComponent;
@@ -13,10 +13,30 @@ describe('VoterComponent', () => {
 
     fixture = TestBed.createComponent(VoterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //Too test <span class="vote-count">{{ totalVotes }}</span>
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let debugElement = fixture.debugElement.query(By.css('.vote-count'));
+    let element = debugElement.nativeElement;
+
+    expect(element.innerText).toContain(21); //To Contain as this is a string
+  });
+
+  // To test [class.highlighted]="myVote == 1"
+  it('should highlight the upvote button', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let debugElement = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    expect(debugElement.classes['highlighted']).toBeTruthy();
+  })
 });
