9b4985243481f9e5605121d407dbd3aa205eb335
JoseMMorales <josemulamorales33@hotmail.com>
2022-04-17 17:29:43 +0200

    Testing RouterOutlet Components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterLinkWithHref, RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ RouterTestingModule.withRoutes([]) ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
       ],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'testing-demo-Angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('testing-demo-Angular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testing-demo-Angular app is running!');
+  it('should have a router outlet', () => {
+    let de = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(de).not.toBeNull();
   });
+
+  it('should have a link to todos page', () => {
+    //<a href="/todos"> in routerLink
+    //One Way
+    let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    let index = debugElements.findIndex(de => de.properties['href'] === '/todos');
+    expect(index).toBeGreaterThan(-1);
+
+    //Another Way
+    let href = fixture.debugElement.query(By.css('a')).nativeElement.getAttribute('href');
+    expect(href).toEqual('/todos');
+  });
+
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoService } from './08-todos.Integration/todo.service';
+import { HomeComponent } from './home/home.component';
+import { TodosComponent } from './08-todos.Integration/todos.component';
+import { UserDetailsComponent } from './09-user-details-integration/user-details.component';
+import { VoterComponent } from './07-voter-integration/voter.component';
+import { UsersComponent } from './users/users.component';
+
+
+import { routes } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponent,
+    HomeComponent,
+    TodosComponent,
+    UserDetailsComponent,
+    VoterComponent,
+    UsersComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [ TodoService ],
   bootstrap: [AppComponent]
